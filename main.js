d870dd1d13b5c4b3eb8b045ffb3c65ca1be810c9
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-07-18 19:07:29 +1200

    start af space race

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+// Make a scrolling background
 
+// make asteroid and coin elements randomly appear and scroll with background
+
+// position ship on screen
+
+// allow movement with arrow keys
+
+// create object collisions that either end the game or give you points
+
+// increase scroll speed
+
+// count points by time played
